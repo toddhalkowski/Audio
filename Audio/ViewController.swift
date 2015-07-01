a1946918151b302e68e06f36239eb0f5200fde79
Todd Halkowski <thalkowski@virtusa.com>
2015-06-30 20:58:07 -0500

    Audio

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
 
+    @IBAction func Play(sender: AnyObject) {
+        
+        var audioPath = NSBundle.mainBundle().pathForResource("r_allemande", ofType: "mp3")!
+        
+            var error : NSError? = nil
+        
+        player = AVAudioPlayer(contentsOfURL:NSURL(string: audioPath), error: &error)
+        
+        if error == nil {
+            
+            player.play()
+            
+        } else {
+            
+            println(error)
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func Pause(sender: AnyObject) {
+        
+        
+        player.pause()
+        
+        
+    }
+    
+    
+    @IBAction func sliderChanged(sender: AnyObject) {
+        
+        player.volume = sliderValue.value 
+        
+        
+        
+    }
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
